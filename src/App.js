c7fc12b0c6abd34667734e0cb0842b5782677fbd
Renan Alfedro Rosales <alfredorg9306@gmail.com>
2024-12-17 11:01:57 -0600

    Fixing After Cherry Picking

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+
+  const [count, setCount] = useState(0);
+
+  function Increment() {
+    setCount(prevCount => prevCount + 1);
+  }
+
+  // function Increment() {
+  //   setCount(count + 1 );
+  // }
+
+  function Descrement(){
+    setCount (prevCount => prevCount - 1);
+  }
+
+
   return (
     <div className="App">
-      
+        <h1>{count}</h1>
+        <button onClick={Increment}>Plus 1</button>
+        <button onClick={Descrement}>Minus 1</button>      
+
     </div>
   );
 }
