a54699b23269d088d889c10bef3b8b343e918975
Renan Alfedro Rosales <alfredorg9306@gmail.com>
2024-12-17 10:50:59 -0600

    Adding the plus 1 button and functionallity

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function Increment() {
+    setCount(prevCount => prevCount + 1);
+  }
+
+  // function Increment() {
+  //   setCount(count + 1 );
+  // }
+
+
+
+
   return (
     <div className="App">
-      
+        <h1>{count}</h1>
+        <button onClick={Increment}>Plus 1</button>      
     </div>
   );
 }
